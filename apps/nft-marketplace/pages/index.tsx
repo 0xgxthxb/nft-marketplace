6d231a5fe36750a833ab8526fcfc265726204881
Paul Krishnamurthy <paul@paulkr.com>
2022-04-08 11:59:14 -0400

    Initial

---
+++ apps/nft-marketplace/pages/index.tsx
@@ -0,0 +1,51 @@
+import ControlledProductGrid from '@nft-marketplace/components/Grids/ProductGrid/controlled/ControlledProductGrid';
+import { Heading, HStack, Link, Text, Image, Box } from '@chakra-ui/react';
+import Head from 'next/head';
+
+export function Index() {
+  return (
+    <>
+      <Head>
+        <title>NFT Marketplace Demo</title>
+      </Head>
+      <Link
+        isExternal
+        href="https://github.com/buildable/nft-marketplace-private"
+      >
+        <Image
+          mr="30px"
+          mt="15px"
+          w="50px"
+          marginLeft="auto"
+          alt="Github Logo"
+          src="https://assets.buildable.dev/catalog/node-templates/github.svg"
+        />
+      </Link>
+      <Heading color="black" w="100%" textAlign="center">
+        NFT Marketplace Demo
+      </Heading>
+      <HStack mt="3" justify="center" w="100%">
+        <Text color="black" fontWeight="semibold" fontSize="lg">
+          Powered by
+        </Text>
+        <Link isExternal href="https://buildable.dev">
+          <Image
+            alt="Buildable logo"
+            w="100px"
+            src="/images/buildableLogo.svg"
+          />
+        </Link>
+      </HStack>
+      <Box
+        maxW="1500px"
+        mx="auto"
+        px={{ base: '4', md: '8', lg: '12' }}
+        py={{ base: '6', md: '8', lg: '12' }}
+      >
+        <ControlledProductGrid />
+      </Box>
+    </>
+  );
+}
+
+export default Index;
